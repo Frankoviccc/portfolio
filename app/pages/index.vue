e5abe3d49fdc3624dfcc1360e5f6d6fd09013f73
frankstruik <frank_struikk@hotmail.com>
2025-10-05 10:17:22 +0200

    More setup

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>
+        Home
+    </h1>
+
+    <FskButton
+        :is="NuxtLink"
+        type="primary"
+        :background="true"
+    >
+        View my work
+    </FskButton>
+</template>
+<script setup lang="ts">
+import FskButton from "~/components/atoms/button/fsk-button.vue";
+import NuxtLink from '#app/components/nuxt-link.js'
+</script>
