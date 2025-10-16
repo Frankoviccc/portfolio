83d478853b44a97e78dc0ecd063cb25e495d0116
frankstruik <frank_struikk@hotmail.com>
2025-10-16 19:17:09 +0200

    Page header and icons

---
+++ app/components/organisms/headers/page-header/fsk-page-header.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="page-header">
+        <div class="page-header__container bento">
+            <div class="page-header__content">
+                <h1 class="page-header__title">
+                    <span class="u-heading-2">{{ title }}</span>
+                    {{ subTitle }}
+                </h1>
+
+                <p>
+                    {{ text }}
+                </p>
+
+                <ul class="page-header__items">
+                    <li
+                        v-for="(item, index) in items"
+                        :key="index"
+                        class="page-header__item"
+                    >
+                        <component
+                            :is="item.icon"
+                        />
+
+                        <span class="u-paragraph">
+                            {{ item.label }}
+                        </span>
+                    </li>
+                </ul>
+
+                <div class="page-header__button-group">
+                    <FskButton
+                        v-for="(item, index) in buttonGroup"
+                        :key="index"
+                        :is="item.is"
+                        :background="item.background"
+                        :icon="item.icon"
+                    >
+                        View my work
+                    </FskButton>
+                </div>
+            </div>
+        </div>
+        <div class="page-header__container bento">
+            <picture>
+                <source type="image/avif" srcset="@/assets/images/tokyo.webp">
+                <source type="image/webp" srcset="@/assets/images/tokyo.webp">
+                <img
+                    src="@/assets/images/tokyo.webp"
+                    alt="Photo Frank in Ginza, Tokyo"
+                    class="page-header__image"
+                >
+            </picture>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import FskButton from "~/components/atoms/button/fsk-button.vue";
+import type { iconType } from '@/components/atoms/button/fsk-button.vue'
+import NuxtLink from '#app/components/nuxt-link.js'
+
+interface Props {
+    title: string;
+    subTitle: string;
+    text: string;
+    items: { icon: string; label: string }[];
+    buttonGroup: { icon: iconType; label: string; link: string; background: boolean; is: 'button' | typeof NuxtLink; }[];
+}
+
+defineProps<Props>();
+</script>
+<style lang="scss">
+@use 'fsk-page-header';
+</style>
