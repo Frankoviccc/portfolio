229d7a10548a80402836ed9faa3262af8509c18c
frankstruik <frank_struikk@hotmail.com>
2025-10-16 19:22:22 +0200

    Icon carousel and icon svgs

---
+++ app/components/organisms/icon-carousel/fsk-icon-carousel.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="icon-carousel">
+        <div class="icon-carousel__inner bento">
+            <h2 class="icon-carousel__title">
+                {{ title }}
+            </h2>
+
+            <div class="icon-carousel__track">
+                <ul
+                    v-if="items.length > 7"
+                    v-for="(list, index) in doubleItems"
+                    :key="index"
+                    class="icon-carousel__list"
+                    :class="[
+                        items.length > 4 ? 'icon-carousel__list--animate' : '',
+                        index % 2 !== 0 ? 'icon-carousel__list--reverse' : ''
+                    ]"
+                >
+                    <li
+                        v-for="(item, itemIndex) in list"
+                        :key="itemIndex"
+                        class="icon-carousel__list-item"
+                    >
+                        <component :is="item.icon" />
+                    </li>
+                </ul>
+
+                <ul
+                    v-else
+                    class="icon-carousel__list"
+                    :class="items.length > 4 ? 'icon-carousel__list--animate' : ''"
+                >
+                    <li
+                        v-for="(item, index) in doubleItems"
+                        :key="index"
+                        class="icon-carousel__list-item"
+                    >
+                        <component :is="item.icon" />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    title: string;
+    items: { icon: Component }[];
+}
+
+const props = defineProps<Props>();
+
+const doubleItems = computed(() => {
+    if (props.items.length < 8) {
+        return [...props.items, ...props.items]
+    }
+
+    if (props.items.length > 7) {
+        return  [
+            [...props.items.slice(0, props.items.length / 2), ...props.items.slice(0, props.items.length / 2)],
+            [...props.items.slice(props.items.length / 2, props.items.length), ...props.items.slice(props.items.length / 2, props.items.length)],
+        ]
+    }
+})
+</script>
+
+<style lang="scss">
+@use 'fsk-icon-carousel';
+</style>
