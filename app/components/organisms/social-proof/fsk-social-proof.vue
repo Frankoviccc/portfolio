0abb1a0acf21825d59cbc09c1a83ccf16e71df12
frankstruik <frank_struikk@hotmail.com>
2025-11-20 15:01:20 +0100

    Social proof component

---
+++ app/components/organisms/social-proof/fsk-social-proof.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="social-proof bento">
+        <h2 class="social-proof__title">
+            {{ title }}
+        </h2>
+
+        <div class="social-proof__icons">
+            <FskIconContainer
+                v-for="(item, index) in icons"
+                :key="index"
+                :item="item"
+            />
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+import type { Props } from './fsk-social-proof.types';
+import FskIconContainer from "~/components/atoms/icon-container/fsk-icon-container.vue";
+
+defineProps<Props>()
+</script>
+
+<style lang="scss">
+@use 'fsk-social-proof';
+</style>
