a9dd156d27bf2f976375a472e2904b5331945dca
frankstruik <frank_struikk@hotmail.com>
2025-12-29 16:16:13 +0100

    Text component

---
+++ app/components/atoms/text/fsk-text.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="text bento">
+        <div
+            class="text__content"
+            v-html="text"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Props } from './fsk-text.types';
+
+defineProps<Props>()
+</script>
+
+<style lang="scss">
+@use 'fsk-text';
+</style>
