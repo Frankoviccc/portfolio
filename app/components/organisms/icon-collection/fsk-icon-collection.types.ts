347699cd96d9685f73c9d1ae258e30c1d8d82f9a
frankstruik <frank_struikk@hotmail.com>
2025-12-29 16:15:55 +0100

    Icon collection

---
+++ app/components/organisms/icon-collection/fsk-icon-collection.types.ts
@@ -0,0 +1,18 @@
+export type Props = {
+    title?: string;
+    direction?: "row" | "column";
+    items: CollectionItem[] | undefined;
+    center?: boolean;
+    square?: boolean;
+    size?: "sm" | "md" | "lg";
+    responsive?: boolean;
+    color?: string;
+}
+
+export interface CollectionItem {
+    icon: string | Component;
+    color?: string;
+    link?: string;
+    label?: string;
+    image?: string;
+}
---
+++ app/components/organisms/icon-collection/fsk-icon-collection.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="icon-collection bento">
+        <div class="icon-collection__inner">
+            <h2
+                v-if="title"
+                class="icon-collection__title"
+                v-html="title"
+            />
+
+            <slot name="title" />
+
+            <div
+                :class="[
+                    'icon-collection__icons',
+                    responsive && 'icon-collection__icons--responsive',
+                    `icon-collection__icons--${direction}`,
+                    { 'icon-collection__icons--square': square }
+                ]"
+            >
+                <FskIconContainer
+                    v-for="(item, index) in items"
+                    :key="index"
+                    :to="item.link"
+                    :icon="item.icon"
+                    :image="item.image"
+                    :label="item.label"
+                    :icon-color-item="item.color"
+                    :icon-color="color"
+                    :size="size"
+                    :center="center"
+                />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+import type { Props } from './fsk-icon-collection.types';
+import FskIconContainer from "~/components/atoms/icon-container/fsk-icon-container.vue";
+
+withDefaults(defineProps<Props>(), {
+    direction: "row",
+})
+</script>
+
+<style lang="scss">
+@use 'fsk-icon-collection';
+</style>
