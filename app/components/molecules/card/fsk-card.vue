c234b02856473363c51014ca7dffdfdfcb5087ce
frankstruik <frank_struikk@hotmail.com>
2025-12-29 16:29:05 +0100

    Card collection component

---
+++ app/components/molecules/card/fsk-card.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="card bento">
+        <NuxtLink
+            :to="link"
+            class="card__link"
+        >
+            <div class="card__inner">
+
+                <NuxtPicture
+                    v-if="image"
+                    :src="image.src"
+                    :alt="image.alt"
+                    :class="[
+                        'card__image',
+                        { 'card__image--filter': filterImage }
+                    ]"
+                />
+
+                <div class="card__content">
+                    <div class="card__content-header">
+                        <p class="card__content-title">
+                            {{ title }}
+                        </p>
+
+                        <p
+                            v-if="year"
+                            class="card__content-year u-tiny-text"
+                        >
+                            {{ year }}
+                        </p>
+
+                        <div
+                            v-else
+                            class="card__button"
+                        >
+                            <Icon
+                                name="tabler:arrow-up-right"
+                                class="card__button-icon"
+                            />
+                        </div>
+                    </div>
+
+                    <div class="card__content-footer">
+                        <ul
+                            v-if="items"
+                            class="card__content-items"
+                        >
+                            <li
+                                v-for="(item, index) in items"
+                                :key="index"
+                                class="card__content-item"
+                            >
+                                {{ item.label }}
+                            </li>
+                        </ul>
+
+                        <div
+                            v-if="year"
+                            class="card__button"
+                        >
+                            <Icon
+                                name="tabler:arrow-up-right"
+                                class="card__button-icon"
+                            />
+                        </div>
+
+                        <p
+                            v-if="text"
+                            class="card__content-text"
+                            v-html="text"
+                        />
+                    </div>
+                </div>
+            </div>
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Props } from './fsk-card.types';
+
+defineProps<Props>()
+</script>
+
+<style lang="scss">
+@use 'fsk-card';
+</style>
---
+++ app/components/organisms/card-collection/fsk-card-collection.vue
@@ -0,0 +1,51 @@
+<template>
+    <section
+        :class="[
+            'card-collection',
+            { 'card-collection--background': isSwipe }
+        ]"
+    >
+        <h2
+            v-if="title"
+            v-html="title"
+            class="card-collection__title"
+        />
+
+        <div
+            :class="[
+                'card-collection__cards',
+                { 'card-collection__cards--swipe': isSwipe }
+            ]"
+        >
+            <FskCard
+                v-for="(card, index) in cards"
+                :key="index"
+                :filterImage="filterImage"
+                :link="`${pathName}/${card.slug}`"
+                :image="card.image"
+                :title="card.title"
+                :text="card.text"
+                :year="card.year"
+                :items="card.items"
+            />
+        </div>
+
+        <FskButton
+            v-if="buttonLabel && buttonLink"
+            :to="buttonLink"
+            class="card-collection__button"
+        >
+            {{ buttonLabel }}
+        </FskButton>
+    </section>
+</template>
+
+<script setup lang="ts">
+import type { Props } from './fsk-card-collection.types'
+
+defineProps<Props>()
+</script>
+
+<style lang="scss">
+@use 'fsk-card-collection';
+</style>
