36025b7c2c5685628b2a6936f0712f9953a4a4d1
frankstruik <frank_struikk@hotmail.com>
2025-12-29 16:28:54 +0100

    Step collection component

---
+++ app/components/molecules/step/fsk-step.vue
@@ -0,0 +1,31 @@
+<template>
+    <li class="step">
+        <span class="step__number">
+            {{ number }}
+        </span>
+
+        <div class="step__content">
+            <h3 class="step__content-title">
+                {{ title }}
+            </h3>
+
+            <p class="step__content-text">
+                {{ text }}
+            </p>
+        </div>
+    </li>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    number: string;
+    title: string;
+    text: string;
+}
+
+defineProps<Props>()
+</script>
+
+<style lang="scss">
+@use 'fsk-step';
+</style>
---
+++ app/components/organisms/step-collection/fsk-step-collection.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="step-collection bento">
+        <div class="step-collection__inner">
+            <div class="step-collection__header">
+                <h2 class="step-collection__title">
+                    {{ title }}
+                </h2>
+
+                <FskButton
+                    :to="buttonLink"
+                    type="primary"
+                    class="step-collection__button"
+                >
+                    {{ buttonText }}
+                </FskButton>
+            </div>
+
+            <ul class="step-collection__items">
+                <FskStep
+                    v-for="(item, index) in items"
+                    :key="index"
+                    :number="item.number"
+                    :title="item.title"
+                    :text="item.text"
+                />
+            </ul>
+
+            <FskButton
+                :to="buttonLink"
+                type="primary"
+                class="step-collection__button-mobile"
+            >
+                {{ buttonText }}
+            </FskButton>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+import type { Props } from './fsk-step-collection.types'
+
+defineProps<Props>()
+</script>
+
+<style lang="scss">
+@use 'fsk-step-collection';
+</style>
